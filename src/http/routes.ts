d35777657b3f4fcdacf7b246f15aa8833185bb8a
cristhian <cristhianfernandolp@gmail.com>
2025-11-23 18:28:25 -0300

    chore: move files for users controller

--- src/http/routes.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from "fastify";
-import { registerUser } from "./controllers/register";
-import { authenticateUser } from "./controllers/authenticate";
-
-export async function appRoutes(app: FastifyInstance) {
-  app.post('/users', registerUser)
-  app.post('/sessions', authenticateUser)
-}
