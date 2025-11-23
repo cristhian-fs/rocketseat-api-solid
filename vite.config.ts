35e38b8a732bf6b17af04cb593517d87364252af
cristhian <cristhianfernandolp@gmail.com>
2025-11-23 18:28:03 -0300

    feat: setup testing for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig } from "vitest/config"
 import tsconfigPaths from "vite-tsconfig-paths"
+import path from 'node:path'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()]
+  plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, 'src')
+    },
+  },
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/services'
+        }
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment: './prisma/vitest-environment-prisma/prisma-test-environment.ts'
+        }
+      }
+    ]
+  }
 })
